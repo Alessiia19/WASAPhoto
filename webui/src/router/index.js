a52862dbcd1e7b0ff60f89fd19fe592aeaad5b0f
Alessia <alessiacastronovo19@gmail.com>
2024-03-27 23:11:56 +0100

    Created LoginView.vue

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,14 +1,22 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
-		{path: '/', component: HomeView}, // '/' significa che è la prima schermata che vedi una volta runnato, es. login
+		{ 
+			path: '/',
+			component: LoginView 	// Login screen
+		}, 
+    	{ 
+			path: '/home',
+			component: HomeView 	// Home screen
+		}, 
 		{path: '/link1', component: HomeView},
 		{path: '/link2', component: HomeView},
 		{path: '/some/:id/link', component: HomeView},
 	]
 })
 
-export default router
+export default router	
